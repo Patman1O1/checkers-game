4e999d9211a2f31c087e41a3f97d699ff7fe0e99
Patman1O1 <patrickthomas101x@gmail.com>
2026-04-09 13:47:05 -0500

    Add PlayerFile.java

---
+++ server/src/main/java/edu/uic/cs342/project3/backend/PlayerFile.java
@@ -0,0 +1,26 @@
+package edu.uic.cs342.project3.backend;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+
+public class PlayerFile extends File {
+    // ── Fields ───────────────────────────────────────────────────────────────────────────────────────────────────────
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+
+    // ── Constructors ─────────────────────────────────────────────────────────────────────────────────────────────────
+    public PlayerFile(String pathname) throws NullPointerException { super(pathname); }
+
+    // ── Methods ──────────────────────────────────────────────────────────────────────────────────────────────────────
+    public Player read() throws IOException {
+        return PlayerFile.OBJECT_MAPPER.readValue(this, Player.class);
+    }
+
+    public void write(Player player) throws IOException, NullPointerException {
+        if (player == null) {
+            throw new NullPointerException("player is null");
+        }
+        PlayerFile.OBJECT_MAPPER.writerWithDefaultPrettyPrinter().writeValue(this, player);
+    }
+}
