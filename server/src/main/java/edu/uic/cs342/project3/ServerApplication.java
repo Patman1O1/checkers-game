80b4b6f5cf12130af20be054c0a202cde12a8969
Patman1O1 <patrickthomas101x@gmail.com>
2026-04-08 21:48:30 -0500

    Apply FXML and CSS

--- server/src/main/java/edu/uic/cs342/project3/ServerApplication.java
+++ server/src/main/java/edu/uic/cs342/project3/ServerApplication.java
@@ -1,8 +1,13 @@
 package edu.uic.cs342.project3;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.net.URL;
+import java.util.Objects;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -10,6 +15,9 @@ public class ServerApplication extends Application {
     // ── Fields ───────────────────────────────────────────────────────────────────────────────────────────────────────
     private static final Logger LOGGER = Logger.getLogger(ServerApplication.class.getName());
 
+    private static final URL FXML = ServerApplication.class.getResource("/fxml/server.fxml");
+
+    private static final URL CSS = ServerApplication.class.getResource("/css/index.css");
 
     // ── Methods ──────────────────────────────────────────────────────────────────────────────────────────────────────
     public static void main(String[] args) { Application.launch(args); }
@@ -17,7 +25,19 @@ public class ServerApplication extends Application {
     @Override
     public void start(Stage primaryStage) {
         try {
+            // Load FXML
+            FXMLLoader fxmlLoader = new FXMLLoader(ServerApplication.FXML);
+            Parent root = fxmlLoader.load();
+            Scene scene = new Scene(root);
 
+            // Apply CSS
+            scene.getStylesheets().add(Objects.requireNonNull(ServerApplication.CSS).toExternalForm());
+
+            // Set the application title
+            primaryStage.setTitle("Checkers");
+
+            // Display the application
+            primaryStage.setScene(scene);
             primaryStage.show();
         } catch (Exception exception) {
             ServerApplication.LOGGER.log(Level.SEVERE, exception.getMessage(), exception);
