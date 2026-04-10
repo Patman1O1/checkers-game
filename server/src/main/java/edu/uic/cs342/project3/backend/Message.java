20e3e58972c69ee1445d63464987cdbf3a1b5374
Patman1O1 <patrickthomas101x@gmail.com>
2026-04-10 18:03:06 -0500

    Add Message.java

---
+++ server/src/main/java/edu/uic/cs342/project3/backend/Message.java
@@ -0,0 +1,15 @@
+package edu.uic.cs342.project3.backend;
+
+import java.io.Serializable;
+
+public class Message implements Serializable {
+    // ── Type ─────────────────────────────────────────────────────────────────────────────────────────────────────────
+    public static enum Type {
+
+    }
+
+    // ── Fields ───────────────────────────────────────────────────────────────────────────────────────────────────────
+    private static final long serialVersionUID = 42L;
+
+
+}
