8c06d795158de01d91da1b32b7d25c956e38cfa2
Patman1O1 <patrickthomas101x@gmail.com>
2026-04-11 01:20:58 -0500

    Implement start()

--- client/src/main/java/edu/uic/cs342/project3/frontend/ClientApplication.java
+++ client/src/main/java/edu/uic/cs342/project3/frontend/ClientApplication.java
@@ -3,8 +3,14 @@ package edu.uic.cs342.project3.frontend;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class ClientApplication extends Application {
     // ── Fields ───────────────────────────────────────────────────────────────────────────────────────────────────────
+    private static final Logger LOGGER = Logger.getLogger(ClientApplication.class.getName());
+
+    private SceneManager sceneManager;
 
     // ── Methods ──────────────────────────────────────────────────────────────────────────────────────────────────────
     public static void main(String[] args) { Application.launch(args); }
@@ -12,9 +18,16 @@ public class ClientApplication extends Application {
     @Override
     public void start(Stage primaryStage) {
         try {
+            this.sceneManager = SceneManager.getInstance();
+            this.sceneManager.setPrimaryStage(primaryStage);
+            this.sceneManager.showLoginScene();
 
+            primaryStage.setTitle("Checkers");
+            primaryStage.show();
         } catch (Exception exception) {
-
+            String exceptionMessage = exception.getMessage();
+            System.err.printf("%s thrown with message \"%s\"\n", Exception.class.getName(), exceptionMessage);
+            ClientApplication.LOGGER.log(Level.SEVERE, exceptionMessage, exception);
         }
     }
 }
