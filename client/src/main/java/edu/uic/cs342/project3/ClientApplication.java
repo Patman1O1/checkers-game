287995e42f41b818c92e55f166562f3da603e711
Patman1O1 <patrickthomas101x@gmail.com>
2026-04-12 19:09:21 -0500

    Add Logger

--- client/src/main/java/edu/uic/cs342/project3/ClientApplication.java
+++ client/src/main/java/edu/uic/cs342/project3/ClientApplication.java
@@ -3,8 +3,11 @@ package edu.uic.cs342.project3;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
+import java.util.logging.Logger;
+
 public class ClientApplication extends Application {
     // ── Fields ───────────────────────────────────────────────────────────────────────────────────────────────────────
+    private static final Logger LOGGER = Logger.getLogger(ClientApplication.class.getName());
 
     // ── Methods ──────────────────────────────────────────────────────────────────────────────────────────────────────
     public static void main(String[] args) { Application.launch(args); }
