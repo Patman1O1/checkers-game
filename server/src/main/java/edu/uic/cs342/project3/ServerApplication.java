2762cb02160567f46e754d72b06377ce663887d2
Patman1O1 <patrickthomas101x@gmail.com>
2026-04-12 19:06:36 -0500

    Add ServerApplication.java

---
+++ server/src/main/java/edu/uic/cs342/project3/ServerApplication.java
@@ -0,0 +1,20 @@
+package edu.uic.cs342.project3;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class ServerApplication extends Application {
+    // ── Fields ───────────────────────────────────────────────────────────────────────────────────────────────────────
+
+    // ── Methods ──────────────────────────────────────────────────────────────────────────────────────────────────────
+    public static void main(String[] args) { Application.launch(args); }
+
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+
+        } catch (Exception exception) {
+
+        }
+    }
+}
