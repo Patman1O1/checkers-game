8d2bc8f10c3911869e57896b55fde82faa19815d
Patman1O1 <patrickthomas101x@gmail.com>
2026-04-19 18:19:34 -0500

    Edit ClientApplication.java

--- client/src/main/java/edu/uic/cs342/ClientApplication.java
+++ client/src/main/java/edu/uic/cs342/ClientApplication.java
@@ -1,30 +1,35 @@
 package edu.uic.cs342;
 
 import edu.uic.cs342.http.ClientThread;
+
 import javafx.application.Application;
 import javafx.stage.Stage;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class ClientApplication extends Application {
+    // ── Fields ───────────────────────────────────────────────────────────────────────────────────────────────────────
+    private static final Logger LOGGER = Logger.getLogger(ClientApplication.class.getName());
 
-    // ── Methods ───────────────────────────────────────────────────────────────
+    // ── Methods ──────────────────────────────────────────────────────────────────────────────────────────────────────
+    public static void main(String[] args) { ClientApplication.launch(args); }
 
-    public static void main(String[] args) {
-        ClientApplication.launch(args);
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            primaryStage.setResizable(false);
+
+            ClientThread client = ClientThread.getInstance();
+            client.start();
+
+            SceneManager sceneManager = SceneManager.create(primaryStage, client);
+            sceneManager.showLogin();
+        } catch (Exception exception) {
+            ClientApplication.LOGGER.log(Level.SEVERE, exception.getMessage(), exception);
+        }
     }
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
-        primaryStage.setResizable(false);
-
-        ClientThread client = ClientThread.getInstance();
-        client.start();
-
-        SceneManager sceneManager = SceneManager.create(primaryStage, client);
-        sceneManager.showLogin();
-    }
-
-    @Override
-    public void stop() {
-        ClientThread.getInstance().close();
-    }
+    public void stop() { ClientThread.getInstance().close(); }
 }
