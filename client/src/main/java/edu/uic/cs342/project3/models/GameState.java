c98c436eeccfd9178b4dfa1cebeb02270ea27de9
Patman1O1 <patrickthomas101x@gmail.com>
2026-04-20 12:44:50 -0500

    Edit GameState.java

--- client/src/main/java/edu/uic/cs342/project3/models/GameState.java
+++ client/src/main/java/edu/uic/cs342/project3/models/GameState.java
@@ -1,4 +1,4 @@
-package edu.uic.cs342.project3.model;
+package edu.uic.cs342.project3.models;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
