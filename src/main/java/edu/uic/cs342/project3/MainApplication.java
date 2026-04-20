1f78f6e7e96653698393fb84765bd268a80a5e4c
Patman1O1 <patrickthomas101x@gmail.com>
2026-04-20 15:26:14 -0500

    Add MainApplication.java

---
+++ src/main/java/edu/uic/cs342/project3/MainApplication.java
@@ -0,0 +1,17 @@
+package edu.uic.cs342.project3;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainApplication extends Application {
+    // ── Methods ──────────────────────────────────────────────────────────────────────────────────────────────────────
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
+}
