a4f9400f5933b5bb788ebe0ec6842204be3e0b70
Patman1O1 <patrickthomas101x@gmail.com>
2026-04-20 15:29:52 -0500

    Add main()

--- src/main/java/edu/uic/cs342/project3/MainApplication.java
+++ src/main/java/edu/uic/cs342/project3/MainApplication.java
@@ -4,7 +4,12 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 
 public class MainApplication extends Application {
+    // ── Fields ───────────────────────────────────────────────────────────────────────────────────────────────────────
+
+
     // ── Methods ──────────────────────────────────────────────────────────────────────────────────────────────────────
+    public static void main(String[] args) { Application.launch(args); }
+
     @Override
     public void start(Stage primaryStage) {
 
