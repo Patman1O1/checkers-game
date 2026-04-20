f36f09bce64b4a484c7acd5d9dfcea8545438387
Patman1O1 <patrickthomas101x@gmail.com>
2026-04-20 00:08:10 -0500

    Edit Color.java

--- server/src/main/java/edu/uic/cs342/model/Color.java
+++ server/src/main/java/edu/uic/cs342/model/Color.java
@@ -4,40 +4,32 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonValue;
 
 public enum Color {
-
     // ── Constants ─────────────────────────────────────────────────────────────
-
-    RED("red"),
-    BLACK("black");
+    RED("red"), BLACK("black");
 
     // ── Fields ────────────────────────────────────────────────────────────────
-
     private final String value;
 
     // ── Constructors ──────────────────────────────────────────────────────────
-
-    Color(String value) {
-        this.value = value;
-    }
+    private Color(String value) { this.value = value; }
 
     // ── Getters ───────────────────────────────────────────────────────────────
-
     @JsonValue
     public String getValue() { return this.value; }
 
     // ── Methods ───────────────────────────────────────────────────────────────
-
-    @JsonCreator
-    public static Color fromValue(String value) {
-        for (Color c : Color.values())
-            if (c.value.equalsIgnoreCase(value)) return c;
-        throw new IllegalArgumentException("Unknown color: " + value);
-    }
-
-    public Color opposite() {
-        return this == Color.RED ? Color.BLACK : Color.RED;
-    }
-
     @Override
     public String toString() { return this.value; }
+
+    @JsonCreator
+    public static Color fromValue(String value) throws IllegalArgumentException {
+        for (Color color : Color.values()) {
+            if (color.value.equalsIgnoreCase(value)) {
+                return color;
+            }
+        }
+        throw new IllegalArgumentException(String.format("Unknown color: %s", value));
+    }
+
+    public Color opposite() { return this == Color.RED ? Color.BLACK : Color.RED; }
 }
