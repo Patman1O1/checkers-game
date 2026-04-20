ccc23647a7f7553a7c84c0fe2c4bb17e87541228
Patman1O1 <patrickthomas101x@gmail.com>
2026-04-20 12:00:53 -0500

    Update package name

--- server/src/main/java/edu/uic/cs342/project3/models/Player.java
+++ server/src/main/java/edu/uic/cs342/project3/models/Player.java
@@ -1,4 +1,4 @@
-package edu.uic.cs342.models;
+package edu.uic.cs342.project3.models;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
